f4888a57483c9693ecc8e68f3a635a118877ce4f
Burak <burakkaya_2004@hotmail.com>
2024-06-15 20:32:36 +0300

    feat: Add Intern class implementation

---
+++ ex03/main.cpp
@@ -0,0 +1,86 @@
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "Intern.hpp"
+
+int main(void)
+{
+	{
+		std::cout << "\033[34mConstructing\033[0m" << std::endl;
+		Bureaucrat *a = new Bureaucrat();
+		Intern *z = new Intern();
+		AForm *b = z->makeForm("SomeRandomForm", "Clown");
+		b = z->makeForm("PresidentialPardonForm", "Clown");
+		// Form *b = new PresidentialPardonForm("Clown");
+		// Form *c = new RobotomyRequestForm("Bender");
+		// Form *d = new ShrubberyCreationForm("Christmas");
+		std::cout << std::endl;
+
+		std::cout << "\033[34mTesting\033[0m" << std::endl;
+		std::cout << a;
+		std::cout << b;
+			std::cout << "debug: " << std::endl;
+		std::cout << std::endl;
+
+		try
+		{
+			b->beSigned(*a);
+			// b->execute(*a);
+		}
+		catch(AForm::GradeTooLowException &e)
+		{
+			std::cerr << "\033[33m" << a->getName() << " was not able to sign " << b->getName() << ": " << e.what() << "\033[0m" << std::endl;
+		}
+
+		std::cout << std::endl;
+		std::cout << b;
+		std::cout << std::endl;
+
+		std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
+		delete a;
+		delete b;
+		delete z;
+		std::cout << std::endl;
+	}
+	std::cout << "-------------------------------------------------" << std::endl;
+	{
+		std::cout << std::endl;
+
+		std::cout << "\033[34mConstructing\033[0m" << std::endl;
+		Bureaucrat *a = new Bureaucrat("Emperor", 1);
+		Intern *z = new Intern();
+		AForm *b = z->makeForm("RobotomyRequestForm", "Bender");
+		AForm *c = z->makeForm("ShrubberyCreationForm", "Cristmas");
+		// Form *c = new ShrubberyCreationForm("christmas");
+		std::cout << std::endl;
+
+		std::cout << "\033[34mTesting\033[0m" << std::endl;
+		std::cout << a;
+		std::cout << b;
+		std::cout << c;
+		std::cout << std::endl;
+
+		b->beSigned(*a);
+		a->signForm(*c);
+		std::cout << std::endl;
+		std::cout << b;
+		std::cout << c;
+		std::cout << std::endl;
+
+		for (int i= 0; i < 10; i++)
+			b->execute(*a);
+		a->executeForm(*c);
+		// c->execute(*a);
+		std::cout << std::endl;
+
+		std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
+		delete a;
+		delete b;
+		delete c;
+		delete z;
+		std::cout << std::endl;
+	}
+	return (0);
+}
